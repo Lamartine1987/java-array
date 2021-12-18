bc35db4fe2f4c24d87b7629b0fd556e015ea391a
Lamartine1987 <lamartinecezar@hotmail.com>
2021-12-18 20:28:39 -0300

    initial commit

---
+++ src/br/com/arrays/ArraysConsoantes.java
@@ -0,0 +1,33 @@
+package br.com.arrays;
+
+import java.util.Scanner;
+
+public class ArraysConsoantes {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        String[] consoantes = new String[4];
+
+        int count = 0, qtd_consoantes = 0;
+
+        do {
+            System.out.println("Digite uma letra: ");
+            String letra = scan.next();
+            if(!(letra.equalsIgnoreCase("a")|
+                    letra.equalsIgnoreCase("e")|
+                    letra.equalsIgnoreCase("i")|
+                    letra.equalsIgnoreCase("o")|
+                    letra.equalsIgnoreCase("u"))){
+                consoantes[count] = letra;
+                qtd_consoantes++;
+            }
+            count++;
+        }while(count < consoantes.length);
+        System.out.println("\nQuantidade de consoantes: " + qtd_consoantes);
+        System.out.print("Consoantes: ");
+        for ( String consoante : consoantes ) {
+            if(consoante != null)System.out.print(consoante + ", ");
+        }
+    }
+}
